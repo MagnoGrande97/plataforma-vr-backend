59fd4ae22b99689fe70dda6b93cac19bc24027ec
Magno Grande <magno1997@hotmail.com>
2026-04-28 01:25:57 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0'); // 👈 ESTA LÍNEA ES LA CLAVE
+
+  console.log('APP RUNNING ON PORT:', port);
 }
 bootstrap();
