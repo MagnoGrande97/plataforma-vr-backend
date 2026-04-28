dc5a3be9e4b41da291c448ac08a798495a3b04a0
Magno Grande <magno1997@hotmail.com>
2026-04-28 02:26:30 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('');
+
   const port = Number(process.env.PORT) || 10000;
 
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
 
   console.log('APP RUNNING ON PORT:', port);
 }
+
 bootstrap();
