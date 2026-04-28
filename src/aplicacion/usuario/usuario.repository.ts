296caf7c0370f6f98076a54b076a946484108327
Magno Grande <magno1997@hotmail.com>
2026-04-28 17:04:08 -0500

    update

--- src/aplicacion/usuario/usuario.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Usuario } from '../../dominio/usuario/usuario.entity';
-
-export interface UsuarioRepository {
-  buscarPorAuth0Id(auth0Id: string): Promise<Usuario | null>;
-  crear(usuario: Usuario): Promise<Usuario>;
-}
--- src/dominio/usuario/usuario.entity.ts
+++
@@ -1,9 +0,0 @@
-export class Usuario {
-  id: string;
-  auth0Id: string;
-  email: string;
-  nombre: string;
-  institucionId?: string | null;
-  rol?: string | null;
-  creadoEn: Date;
-}
