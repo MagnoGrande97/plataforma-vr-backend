2dfb4b2162487124c5a36f1895a8c872290d9f71
Magno Grande <magno1997@hotmail.com>
2026-04-28 02:14:14 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
 
-  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
 
-  await app.listen(port, '0.0.0.0');
+  const port = process.env.PORT || 10000;
 
-  console.log('APP RUNNING ON PORT:', port);
+  await app.init();
+  server.listen(port, '0.0.0.0', () => {
+    console.log('APP RUNNING ON PORT:', port);
+  });
 }
+
 bootstrap();
